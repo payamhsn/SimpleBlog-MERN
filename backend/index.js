e113245f5ccc1bac06597efc292c31927d427a6c
Payam H. <p.hoseini.ch@gmail.com>
2024-10-15 19:59:03 +0330

    Connect backend to db using mongoose.Install dotenv and cors.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = process.env.PORT || 3000;
+
+// parse options
+app.use(express.json());
+app.use(cors());
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+}
+
+main()
+  .then(() => console.log("Connected to DB."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
