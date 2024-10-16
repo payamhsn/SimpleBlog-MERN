c715495c626f27f31ffeb054656164280a2cab95
Payam H. <p.hoseini.ch@gmail.com>
2024-10-16 16:16:51 +0330

    Make a rote for crating a post and send that to db.

--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
+const Blog = require("../model/blog.model");
+
+// create a blog post
+router.post("/create-post", async (req, res) => {
+  try {
+    const newPost = new Blog({ ...req.body });
+    await newPost.save();
+    res.status(201).send({
+      message: "Post created successfully",
+      post: newPost,
+    });
+  } catch (error) {
+    console.error("Error creating post: ", error);
+    res.status(500).send({ message: "Error creating post. " + error.message });
+  }
+});
+
 router.get("/", (req, res) => {
   res.send("Hello from blog route");
 });
