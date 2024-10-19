d401946bdec506cde17466526e8ea2742e67bcb8
Payam H. <p.hoseini.ch@gmail.com>
2024-10-19 19:12:38 +0330

    Create hamburger menu for mobile devices.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { IoClose, IoMenuSharp } from "react-icons/io5";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   const navLists = [
     { name: "Home", path: "/" },
     { name: "About us", path: "/about-us" },
@@ -14,6 +19,7 @@ const Navbar = () => {
       <nav className="container mx-auto flex justify-between px-5">
         <Link to="/">
           <img src="/logo.png" alt="" className="h-12" />
+          <p>Logo here</p>
         </Link>
         <ul className="sm:flex hidden items-center gap-8">
           {navLists.map((list, index) => (
@@ -27,6 +33,37 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        {/* toogle menu */}
+        <div className="flex items-center sm:hidden">
+          <button
+            onClick={toggleMenu}
+            className="flex items-center px-3 py-4 bg-[#fafafa] rounded text-sm text-gray-500 hover:text-gray-900"
+          >
+            {isMenuOpen ? (
+              <IoClose className="size-6" />
+            ) : (
+              <IoMenuSharp className="size-6" />
+            )}
+          </button>
+        </div>
+
+        {/* mobile menu items */}
+        {isMenuOpen && (
+          <ul className="fixed top-[106px] left-0 w-full h-auto pb-8 border-b bg-white shadow-sm z-50 sm:hidden">
+            {navLists.map((list, index) => (
+              <li className="mt-5 px-4" key={index}>
+                <NavLink
+                  onClick={() => setIsMenuOpen(false)}
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                  to={`${list.path}`}
+                >
+                  {list.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        )}
       </nav>
     </header>
   );
