6016bd3f1c433b4332f27b90f725186fc9d3b47a
Payam H. <p.hoseini.ch@gmail.com>
2024-10-20 20:42:23 +0330

    Complete login page functionality. Add RTK for global state management.

--- frontend/src/pages/user/Login.jsx
+++ frontend/src/pages/user/Login.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useLoginUserMutation } from "../../redux/features/auth/authApi";
+import { setUser } from "../../redux/features/auth/authSlice";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+
+  const [loginUser, { isLoading: loginLoading }] = useLoginUserMutation();
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await loginUser(data).unwrap();
+      const { token, user } = response;
+      dispatch(setUser({ user }));
+      alert("Login successful");
+      navigate("/");
+    } catch (error) {
+      setMessage("Please provide a valid email and password");
+    }
+  };
 
   return (
     <div className="max-w-sm bg-white mx-auto px-3 sm:p-8 mt-24">
       <h2 className="text-2xl font-semibold">Login</h2>
 
-      <form className="space-y-5 max-w-sm mx-auto pt-8">
+      <form onSubmit={handleLogin} className="space-y-5 max-w-sm mx-auto pt-8">
         <input
           type="text"
           value={email}
@@ -29,7 +55,10 @@ const Login = () => {
         {
           message && <p className="text-red-500">{message}</p> // Display error message if any
         }
-        <button className="w-full mt-5 bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded-md">
+        <button
+          disabled={loginLoading}
+          className="w-full mt-5 bg-primary hover:bg-indigo-500 text-white font-medium py-3 rounded-md"
+        >
           Login
         </button>
       </form>
