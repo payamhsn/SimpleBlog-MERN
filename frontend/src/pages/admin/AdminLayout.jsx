e115659abbd493d11f2c770e870beaf3886002f5
Payam H. <p.hoseini.ch@gmail.com>
2024-10-21 14:34:39 +0330

    Create admin dashboard ui structure.

---
+++ frontend/src/pages/admin/AdminLayout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import AdminNavigation from "./AdminNavigation";
+
+const AdminLayout = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  if (!user || user.role !== "admin") {
+    // alert("You must be a admin!");
+    // Redirect to home or login if the user is not an admin
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <div className="container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start">
+      <header className="lg:w-1/5 sm:2/5 w-full">
+        <AdminNavigation />
+      </header>
+
+      <main className="p-8 bg-white w-full">
+        <p>Contents here</p>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AdminLayout;
