0897597e3687e011e7272ff183befbfa91cf2373
Payam H. <p.hoseini.ch@gmail.com>
2024-10-21 12:48:11 +0330

    Complete navbar login and logout ui and functionality.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { IoClose, IoMenuSharp } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutUserMutation } from "../redux/features/auth/authApi";
+import { logout } from "../redux/features/auth/authSlice";
+
+import AvaterImg from "../assets/commentor.png";
+
+const navLists = [
+  { name: "Home", path: "/" },
+  { name: "About us", path: "/about-us" },
+  { name: "Privacy Policy", path: "/privacy-policy" },
+  { name: "Contact Us", path: "/contact-us" },
+];
 
 const Navbar = () => {
+  const dispatch = useDispatch();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { user } = useSelector((state) => state.auth);
+  const [logoutUser] = useLogoutUserMutation();
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
-  const navLists = [
-    { name: "Home", path: "/" },
-    { name: "About us", path: "/about-us" },
-    { name: "Privacy Policy", path: "/privacy-policy" },
-    { name: "Contact Us", path: "/contact-us" },
-  ];
+  const handleLogout = async () => {
+    try {
+      await logoutUser().unwrap();
+      dispatch(logout());
+    } catch (err) {
+      console.error("Failed to logout:", err);
+    }
+  };
 
   return (
     <header className="bg-white py-6 border">
@@ -32,9 +49,33 @@ const Navbar = () => {
               </NavLink>
             </li>
           ))}
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
+          {user && user?.role === "user" ? (
+            <li className="flex gap-3 items-center">
+              <img src={AvaterImg} alt="" className="size-8" />
+              <button
+                onClick={handleLogout}
+                className="bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm"
+              >
+                Logout
+              </button>
+            </li>
+          ) : (
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          )}
+
+          {/* admin */}
+          {user && user?.role === "admin" && (
+            <li className="flex gap-3 items-center">
+              <img src={AvaterImg} alt="" className="size-8" />
+              <Link to="/dashboard">
+                <button className="bg-[#1E73BE] px-4 py-1.5 text-white rounded-sm">
+                  Dashboard
+                </button>
+              </Link>
+            </li>
+          )}
         </ul>
 
         {/* toogle menu */}
@@ -65,6 +106,18 @@ const Navbar = () => {
                 </NavLink>
               </li>
             ))}
+            {user ? (
+              <>
+                <img src={AvaterImg} alt="" className="size-8" />
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <li className="px-4 mt-5">
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            )}
           </ul>
         )}
       </nav>
